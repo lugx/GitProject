ee6c58fdc4accdad68f75f970bbe9548ec686edc
Guoxian Lu <vip_lgx@126.com>
2012-04-16 23:15:58 +0800

    Change the helloworld

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+ *This is a simple project that give me the example to 
+ *learn the project that well suited to the projects.
+ */
+
 int main(int argc,char **argv){
   printf("Hello,world!\n");
   return 0;
